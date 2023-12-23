0ad9932fc590ac820eded5517b9757f1b3e7f4f2
EmanuelTeklu <109396531+EmanuelTeklu@users.noreply.github.com>
2023-12-23 17:44:21 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+// Redirect to homepage on name click
+const nameEl = document.getElementById('name');
+nameEl.addEventListener('click', () => {
+  window.location.href = '/'; // Replace '/' with your homepage URL if different
+});
